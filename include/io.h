75c32c92ace9a36a137b06679eeb637a4d8304c8
rkbrogan <riley.k.brogan@gmail.com>
2022-09-29 01:38:46 +0000

    Started io

---
+++ include/io.h
@@ -0,0 +1,22 @@
+#ifndef IO_H
+#define IO_H
+
+#include "Code.h"
+
+#include <stdint.h>
+
+int read_bytes(int infile, uint8_t *buf, int nbytes);
+
+int write_bytes(int outfile, uint8_t *buf, int nbytes);
+
+bool read_bit(int infile, uint8_t *bit);
+
+void write_code(int outfile, Code* c);
+
+void flush_codes(int outfile);
+
+// TODO: Create pseudo code for read and write bytes
+// TODO: Find binary editor to create binary files to test
+// Test by comparing files
+
+#endif
