84d57c95b06a74ed373ca02d2a27f7833240f774
rkbrogan <riley.k.brogan@gmail.com>
2022-10-17 00:07:07 +0000

    added some code for read_bytes

--- src/DataStructures/IO.c
+++ src/DataStructures/IO.c
@@ -1,9 +1,27 @@
 #include "IO.h"
 
+#include <fcntl.h>
+#include <unistd.h>
+
+// TODO: Find out what to do with these
+// extern bytes_read;
+// extern bytes_written;
+
 // Wrapper function to perform reads.
 int read_bytes(int infile, uint8_t *buf, int nbytes)
 {
-    return 1;
+    int n = 0;
+
+    do
+    {
+        // Try to read remaining bytes
+        n = read(infile, &buf[n], nbytes);
+
+        // Adjust remaining amount
+        nbytes -= n;
+    } while (n != 0);
+    
+    return nbytes;
 }
 
 int write_bytes(int outfile, uint8_t *buf, int nbytes)
--- test/IO_tests.c
+++ test/IO_tests.c
@@ -12,20 +12,36 @@
 // Test 0: Read the test binary file
 static MunitResult io_test_linux_read(const MunitParameter params[], void* data)
 {
-    // uint8_t bits[MAX_CODE_SIZE];
-    char test_bits[MAX_CODE_SIZE];
+    uint8_t test_bits[MAX_CODE_SIZE];
 
+    // Generated from https://www.dcode.fr/file-data
     int f1 = open("./test/dcode-file_01010101", O_RDONLY);
-    // read(fileDescriptor, buffer, bytesCount)
-    int n1 = read(f1, test_bits, 8);
 
-    printf("\t %d", n1);
+    // read(fileDescriptor, buffer, bytesCount)
+    int n1 = read(f1, test_bits, 1);
+    munit_assert_int(close(f1), ==, 0);
+    
+    munit_assert_int(n1, ==, 1);
 
     return MUNIT_OK;
 }
 
+// Test: Read a file smaller than BLOCK (4 KB)
+static MunitResult io_read_bytes_3kb(const MunitParameter params[], void* data)
+{
+    int inFile = open("", O_RDONLY);
 
+    uint8_t buffer[3*1024];
 
+    int result = read_bytes(inFile, buffer, sizeof(buffer));
+    munit_assert_int(close(inFile), ==, 0);
+
+    munit_assert_int(result, ==, sizeof(buffer));
+
+    // Loop and check values of buffer
+
+    return MUNIT_OK;
+}
 
 
 /**************************************************************/
